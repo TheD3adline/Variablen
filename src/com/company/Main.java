d8799400bf4cd84f4218f43470628db8aa69b4d8
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-08 12:08:13 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int age = 35;   //Initialisierung
+
+        int age2;       //Deklaration
+        age2 = 37;      //Zuweisung
+
+        System.out.println(age2);
+
+        age2 = 30;
+
+        System.out.println(age2);
+
+    }
+}
